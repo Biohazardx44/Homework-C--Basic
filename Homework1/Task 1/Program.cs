b990e52e8caed5d0afa4ea208534940788d16524
Biohazardx44 <nikolailievski420@gmail.com>
2023-03-10 17:41:11 +0100

    Calculator & Average Exercises

---
+++ Homework1/Task 1/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Using double just in case for decimals
+
+////With Switch-Case just in case :)
+//Console.WriteLine("This is a Real Calculator!");
+//Console.Write("Enter the First number: ");
+//double num1 = Convert.ToDouble(Console.ReadLine());
+
+//Console.Write("Enter the Second number: ");
+//double num2 = Convert.ToDouble(Console.ReadLine());
+
+//Console.Write("Enter the Operation (+, -, *, /): ");
+//string operation = Console.ReadLine();
+
+//double result = 0;
+//switch (operation)
+//{
+//    case "+":
+//        result = num1 + num2;
+//        break;
+//    case "-":
+//        result = num1 - num2;
+//        break;
+//    case "*":
+//        result = num1 * num2;
+//        break;
+//    case "/":
+//        result = num1 / num2;
+//        break;
+//    default:
+//        Console.WriteLine("Invalid Operation");
+//        break;
+//}
+
+//Console.WriteLine("The result is: " + result);
+
+//With If-Else
+Console.WriteLine("This is a Real Calculator!");
+Console.Write("Enter the First number: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the Second number: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the Operation (+, -, *, /): ");
+string operation = Console.ReadLine();
+
+double result = 0;
+if (operation == "+")
+{
+    result = num1 + num2;
+}
+else if (operation == "-")
+{
+    result = num1 - num2;
+}
+else if (operation == "*")
+{
+    result = num1 * num2;
+}
+else if (operation == "/")
+{
+    result = num1 / num2;
+}
+else
+{
+    Console.WriteLine("Invalid Operation");
+}
+
+Console.WriteLine("The result is: " + result);
---
+++ Homework1/Task 2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Using double just in case for decimals
+
+Console.WriteLine("This app will determine the average number!");
+
+Console.Write("Enter the First number: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the Second number: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the Third number: ");
+double num3 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the Fourth number: ");
+double num4 = Convert.ToDouble(Console.ReadLine());
+
+double average = (num1 + num2 + num3 + num4) / 4;
+Console.WriteLine("The average of " + num1 + ", " + num2 + ", " + num3 + " and " + num4 + " is: " + average);
